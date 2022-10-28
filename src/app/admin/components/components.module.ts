a28336e9fbf5eb1e9a8e8b6bed5afee77456977d
ynskr <ynskr@DESKTOP-G355Q9L>
2022-10-28 22:08:29 +0300

    ETicaretCLI- Admin ve UI altyapısı oluşturuldu

---
+++ src/app/admin/components/components.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomerModule } from './customer/customer.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { ProductModule } from './product/product.module';
+
+
+
+@NgModule({
+  declarations: [ ],
+  imports: [
+    CommonModule,
+    CustomerModule,
+    ProductModule,
+    DashboardModule
+  ]
+})
+export class ComponentsModule { }
