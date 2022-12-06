c6ecae60b4e3cbc0f0bd65e6bf60525b7d11a726
ynskrca12 <ynskrca52@outlook.com>
2022-12-06 23:12:28 +0300

    Delete Modal Dialog İle Silme Onaylama

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,10 @@ import { BaseComponent } from './base/base.component';
 import { HttpClientModule } from '@angular/common/http';
 import { DeleteDirective } from './directives/admin/delete.directive';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -1,4 +1,6 @@
 import { Directive, ElementRef, EventEmitter, HostListener, Input, Output, Renderer2 } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DeleteDialogComponent, DeleteState } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 import { ProductService } from 'src/app/services/common/models/product.service';
 
@@ -12,7 +14,8 @@ export class DeleteDirective {
   constructor(
     private element:ElementRef,
     private _renderer:Renderer2,
-    private productService:ProductService
+    private productService:ProductService,
+    public dialog: MatDialog
     ) {
       const img = _renderer.createElement("img")
       img.setAttribute("src","../../../../../assets/delete.png.png")
@@ -27,11 +30,28 @@ export class DeleteDirective {
 
     @HostListener("click")
     async onClick(){
-      const td:HTMLTableCellElement=this.element.nativeElement
-     await this.productService.delete(this.id)
-      $(td.parentElement).fadeOut(2000, () => {
-        this.callback.emit()
-      })
+      this.openDialog(async () =>{
+        const td:HTMLTableCellElement=this.element.nativeElement
+        await this.productService.delete(this.id)
+         $(td.parentElement).animate({
+          opacity:0,
+          left:"+=50",
+          height:"toggle"
+         }, 700, () => {
+          this.callback.emit()
+            })
+         })
+      }
+
+    openDialog(afterClosed:any): void {
+      const dialogRef = this.dialog.open(DeleteDialogComponent, {
+        data: DeleteState.Yes,
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        if(result ==DeleteState.Yes)
+        afterClosed()
+      });
     }
 
 }
