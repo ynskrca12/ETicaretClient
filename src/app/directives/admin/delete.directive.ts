bd2f5335a578beb4d94676e038a2dc25e2bd8af1
ynskrca12 <ynskrca52@outlook.com>
2022-12-10 02:36:15 +0300

    Delete Directive Düzeltmeleri

--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -1,6 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Directive, ElementRef, EventEmitter, HostListener, Input, Output, Renderer2 } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteDialogComponent, DeleteState } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 import { ProductService } from 'src/app/services/common/models/product.service';
 
@@ -14,8 +16,9 @@ export class DeleteDirective {
   constructor(
     private element:ElementRef,
     private _renderer:Renderer2,
-    private productService:ProductService,
-    public dialog: MatDialog
+    private httpClientService:HttpClientService,
+    public dialog: MatDialog,
+    private alertifyService:AlertifyService
     ) {
       const img = _renderer.createElement("img")
       img.setAttribute("src","../../../../../assets/delete.png.png")
@@ -26,21 +29,37 @@ export class DeleteDirective {
     }
 
     @Input() id:string
+    @Input() controller:string
     @Output() callback :EventEmitter<any> = new EventEmitter()
 
     @HostListener("click")
     async onClick(){
       this.openDialog(async () =>{
         const td:HTMLTableCellElement=this.element.nativeElement
-        await this.productService.delete(this.id)
-         $(td.parentElement).animate({
+       // await this.productService.delete(this.id)
+       this.httpClientService.delete({
+        controller:this.controller
+       },this.id).subscribe(data =>{
+        $(td.parentElement).animate({
           opacity:0,
           left:"+=50",
           height:"toggle"
          }, 700, () => {
           this.callback.emit()
+          this.alertifyService.message("Ürün başarıyla silinmiştir.",{
+            dismissOthers:true,
+            messageType:MessageType.Success,
+            position:Position.TopRight
+          })
             })
-         })
+              },(errorResponse:HttpErrorResponse)=>{
+                this.alertifyService.message("Ürün silinemedi.Beklenmeyenn Hata!!",{
+                  dismissOthers:true,
+                  messageType:MessageType.Error,
+                  position:Position.TopRight
+                })
+              })
+       })
       }
 
     openDialog(afterClosed:any): void {
